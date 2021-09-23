622f4bf7f4819591d85fc942d8d7c3ee27f45547
SebagadisK <sebexk3@gmail.com>
2021-09-23 15:58:38 -0700

    Fix print message

--- _pop.c
+++ _pop.c
@@ -1,22 +1,19 @@
+#include "monty.h"
 
 void _pop(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
-	printf("Pop function\n");
 	stack_t *tmp = *stack, *last = NULL;
 	int eraseHead = 0;
 
 	if (*stack == NULL)
 		_errorHandler(EMPTY_STACK);
-
 	while(tmp != NULL)
 	{
 		last = tmp;
 		tmp = tmp->next;
 	}
-
 	if (last == NULL)
 		return;
-
 	if (last->prev != NULL)
 		last->prev->next = NULL;
 	if (last->prev == NULL)
